a07bf6489360a09bc03740a2c43508cf1c0303fd
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2017-03-27 19:11:48 -0300

    Finish first class

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,5 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-}
+    @IBOutlet weak var display: UILabel!
 
+    var userIsInTheMiddleOfTyping = false
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+
+    var displayValue : Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "pi":
+                displayValue = Double.pi
+            case "√":
+                let operand = Double(display.text!)!
+                displayValue = sqrt(operand)
+            default:
+                break
+            }
+        }
+    }
+}
